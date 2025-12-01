3af3f7592a9d61ff8ec5b977ec87f7ece8142df4
Fredrik H <8249+folsen@users.noreply.github.com>
2025-12-01 16:18:59 +0100

    Adding UV to manage python shit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2025
+
+Doing it in Python this year to knock some rust off my Python skills.
--- day1/part2.py
+++ day1/part2.py
@@ -1,9 +1,8 @@
-from collections import deque
 import math
 
 input = []
 with open("input2.txt") as file:
-    input = [int(line.rstrip().replace("L","-").replace("R","")) for line in file]
+    input = [int(line.rstrip().replace("L", "-").replace("R", "")) for line in file]
 
 position = 50
 zeros = 0
@@ -27,6 +26,6 @@ for shift in input:
     else:
         position = position + effective_rotation
 
-    assert(position >= 0)
+    assert position >= 0
 
 print(zeros)
