40f4de834dedb8c6d705b00b5e56d8a434d03311
Fredrik H <8249+folsen@users.noreply.github.com>
2025-12-03 14:33:01 +0100

    Had to refactor to recusion instead of that ugly loop

--- day3/part2.py
+++ day3/part2.py
@@ -4,15 +4,18 @@ with open("input.txt") as file:
 
 
 def reduce(bank, smallest):
+    if len(bank) == 12:
+        return bank
+
     for i in range(len(bank)):
         if i == len(bank) - 1 and len(bank) > 12 and int(bank[i]) in smallest:
-            return bank[:i]
+            return reduce(bank[:i], smallest)
         elif i == len(bank) - 1:
-            return bank
+            return reduce(bank, smallest)
 
         if int(bank[i]) < int(bank[i + 1]):
-            return bank[:i] + bank[i + 1 :]
-    return bank
+            return reduce(bank[:i] + bank[i + 1 :], smallest)
+    return reduce(bank, smallest)
 
 
 p2 = 0
@@ -20,9 +23,7 @@ for bank in lines:
     smallest = list(map(int, list(bank)))
     smallest.sort()
     smallest = set(smallest[: len(bank) - 12])
-    while len(bank) > 12:
-        bank = reduce(bank, smallest)
-
+    bank = reduce(bank, smallest)
     p2 += int(bank)
 
 print(p2)
