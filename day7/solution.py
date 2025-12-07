a83160ecd36483c20222247c7a52bd971558a4cb
Fredrik H <8249+folsen@users.noreply.github.com>
2025-12-07 19:56:28 +0100

    Learned about functools cache, which cleans up my manual implementation of cache a lot

--- day7/solution.py
+++ day7/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 input = []
 with open("input.txt") as file:
     input = [list(line.replace("\n", "")) for line in file]
@@ -19,27 +21,20 @@ for i in range(rows - 1):
 print("P1:", p1)
 
 start = 0
-cache = dict()
 for i in range(cols):
     if input[0][i] == "S":
         start = i
         break
 
 
+@cache
 def step(i, j):
-    if cache.get((i, j)) is not None:
-        return cache[(i, j)]
     if i == rows - 1:
         return 1
     if input[i + 1][j] == "^":
-        lc = step(i + 1, j - 1)
-        rc = step(i + 1, j + 1)
-        cache[(i, j)] = lc + rc
-        return lc + rc
+        return step(i + 1, j - 1) + step(i + 1, j + 1)
     else:
-        c = step(i + 1, j)
-        cache[(i, j)] = c
-        return c
+        return step(i + 1, j)
 
 
 print("P2:", step(0, start))
